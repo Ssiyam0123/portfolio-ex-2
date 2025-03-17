30b1589f5ddbe1fdee112fdc08b95eafbe047f57
Ssiyam0123 <ssiyam563@gmail.com>
2025-03-18 02:50:02 +0600

    fix navbar hero section padding

--- src/components/heroSection/HeroText.jsx
+++ src/components/heroSection/HeroText.jsx
@@ -3,7 +3,7 @@ import { fadeIn } from "../../framerMotion/variants";
 
 const HeroText = () => {
   return (
-    <div className="flex flex-col gap-4 h-full justify-center md:text-left sm:text-center">
+    <div className="flex flex-col gap-4 h-full justify-center md:text-left sm:text-center  md:w-1/2 md:mx-auto">
       <motion.h2
         variants={fadeIn("down", 0.2)}
         initial="hidden"
